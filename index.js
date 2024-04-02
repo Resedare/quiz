f9c08dc75b8d605a6cdf67c896a793dc4925d0f0
Resedare <maks.samox@yandex.ru>
2024-04-02 20:10:28 +0300

    selection quiz menu

--- index.js
+++ index.js
@@ -1,49 +1,216 @@
-const questions = [
+const questionsMoon = [
     {
-        question: "Какой язык работает в браузере?",
-        options: ["Java", "C", "Python", "JavaScript"],
+        question: "Какой период обращения вокруг Земли у Луны?",
+        options: ["Около 24 часов", "Около 7 дней", "Около 27 дней", "Около 365 дней"],
+        correct: 3,
+    },
+    {
+        question: "Как называется самая глубокая точка на Луне?",
+        options: ["Валлис", "Кратер Тихо", "Кратер Коперник", "Южнополярная впадина Айткен"],
         correct: 4,
     },
     {
-        question: "Что означает CSS?",
-        options: [
-            "Central Style Sheets",
-            "Cascading Style Sheets",
-            "Cascading Simple Sheets",
-            "Cars SUVs Sailboats",
-        ],
+        question: "Какой из следующих объектов когда-либо не достигал Луны?",
+        options: ["Аполлон-11", "Луноход-1", "Вояджер-1", "Советский зонд Луна-2"],
+        correct: 3,
+    },
+    {
+        question: "Что такое лунное море?",
+        options: ["Водные запасы под поверхностью Луны", "Равнины затвердевшей лавы", "Кратеры, заполненные водой", "Оптическая иллюзия"],
         correct: 2,
     },
     {
-        question: "Что означает HTML?",
-        options: [
-            "Hypertext Markup Language",
-            "Hypertext Markdown Language",
-            "Hyperloop Machine Language",
-            "Helicopters Terminals Motorboats Lamborginis",
-        ],
-        correct: 1,
+        question: "Как называется явление, когда Земля находится между Солнцем и Луной?",
+        options: ["Солнечное затмение", "Лунное затмение", "Синодический месяц", "Фаза полнолуния"],
+        correct: 2,
     },
     {
-        question: "В каком году был создан JavaScript?",
-        options: ["1996", "1995", "1994", "все ответы неверные"],
+        question: "Что означает термин 'синодический месяц'?",
+        options: ["Время вращения Луны вокруг своей оси", "Время между двумя новолуниями", "Период полной ротации Луны вокруг Земли", "Время, необходимое Луне для возвращения в одну и ту же звездную фазу"],
+        correct: 2,
+    },
+    {
+        question: "Кто из астронавтов Аполлона-11 оставался в космическом корабле, когда другие члены экипажа ходили по Луне?",
+        options: ["Нил Армстронг", "Базз Олдрин", "Майкл Коллинз", "Чарльз Дьюк"],
+        correct: 3,
+    },
+    {
+        question: "Как называется эффект, при котором всегда видим только одну сторону Луны?",
+        options: ["Синхронный ротационный эффект", "Гравитационная блокировка", "Периодическое колебание", "Приливное затухание"],
+        correct: 2,
+    },
+    {
+        question: "Какова примерная температура на дневной стороне Луны?",
+        options: ["100°C", "0°C", "-100°C", "120°C"],
+        correct: 4,
+    },
+    {
+        question: "Что из нижеперечисленного представляет собой лунную реголит?",
+        options: ["Жидкая вода на поверхности Луны", "Пылевое и каменное вещество, покрывающее лунную поверхность", "Газообразные выбросы из лунных кратеров", "Скальные образования, формирующиеся в результате вулканической активности"],
         correct: 2,
     },
 ];
 
+const questionsSpace = [
+    {
+        question: "Какое космическое тело называется 'Красной планетой'?",
+        options: ["Меркурий", "Земля", "Марс", "Венера"],
+        correct: 3,
+    },
+    {
+        question: "Как называется наша галактика?",
+        options: ["Андромеда", "Чумацкий Шлях", "Млечный Путь", "Туманность Ориона"],
+        correct: 3,
+    },
+    {
+        question: "Что из этого является карликовой планетой?",
+        options: ["Европа", "Плутон", "Титан", "Энцелад"],
+        correct: 2,
+    },
+    {
+        question: "Какой процесс является источником энергии Солнца?",
+        options: ["Химический", "Ядерный синтез", "Ядерный распад", "Гравитационный"],
+        correct: 2,
+    },
+    {
+        question: "Какой самый большой объект в Солнечной системе?",
+        options: ["Юпитер", "Солнце", "Сатурн", "Ортово облако"],
+        correct: 2,
+    },
+    {
+        question: "Что такое экзопланета?",
+        options: ["Планета, вылетевшая из солнечной системы", "Планета, расположенная внутри астероидного пояса", "Планета, находящаяся за пределами солнечной системы", "Планета, вращающаяся вокруг двух звезд"],
+        correct: 3,
+    },
+    {
+        question: "Какая планета известна своими кольцами?",
+        options: ["Юпитер", "Нептун", "Уран", "Сатурн"],
+        correct: 4,
+    },
+    {
+        question: "Какое явление происходит, когда звезда взрывается?",
+        options: ["Чёрная дыра", "Сверхновая", "Пульсар", "Нейтронная звезда"],
+        correct: 2,
+    },
+    {
+        question: "Какое расстояние свет проходит за один год?",
+        options: ["Световой год", "Световой месяц", "Световая миля", "Световой день"],
+        correct: 1,
+    },
+    {
+        question: "Как называется теория о начале Вселенной?",
+        options: ["Стационарная модель", "Теория Великого взрыва", "Теория суперструн", "Квантовая теория гравитации"],
+        correct: 2,
+    },
+];
+
+const questionsEarth = [
+    {
+        question: "Какова средняя температура поверхности Земли?",
+        options: ["-15°C", "14°C", "35°C", "58°C"],
+        correct: 2,
+    },
+    {
+        question: "Какая часть Земли покрыта водой?",
+        options: ["51%", "65%", "71%", "85%"],
+        correct: 3,
+    },
+    {
+        question: "Какой газ составляет большую часть атмосферы Земли?",
+        options: ["Кислород", "Азот", "Углекислый газ", "Аргон"],
+        correct: 2,
+    },
+    {
+        question: "Как называется самый большой океан на Земле?",
+        options: ["Атлантический океан", "Индийский океан", "Тихий океан", "Северный Ледовитый океан"],
+        correct: 3,
+    },
+    {
+        question: "Какой слой Земли находится непосредственно под корой?",
+        options: ["Мантия", "Ядро", "Литосфера", "Астеносфера"],
+        correct: 1,
+    },
+    {
+        question: "Какая страна самая большая по площади на Земле?",
+        options: ["Канада", "Китай", "США", "Россия"],
+        correct: 4,
+    },
+    {
+        question: "Как называется тектоническая плита, на которой расположена Индия?",
+        options: ["Евразийская плита", "Индийская плита", "Филиппинская плита", "Африканская плита"],
+        correct: 2,
+    },
+    {
+        question: "Какое явление вызывает сезонные изменения климата на Земле?",
+        options: ["Вращение Земли вокруг Солнца", "Наклон земной оси", "Эллиптическая орбита Земли", "Солнечная активность"],
+        correct: 2,
+    },
+    {
+        question: "Какой процесс приводит к перемещению тектонических плит?",
+        options: ["Термоядерный синтез", "Конвекция в мантии", "Гравитационное притяжение Луны", "Магнитное взаимодействие с Солнцем"],
+        correct: 2,
+    },
+    {
+        question: "Как называется самая высокая гора на Земле?",
+        options: ["Килиманджаро", "Эверест", "Денали", "Эльбрус"],
+        correct: 2,
+    },
+];
+
+const chooseButton = document.querySelectorAll('.button--choose')
 const startButton = document.querySelector('.quiz__button--start');
 const quizMenu = document.querySelector('.quiz__block');
 const startMenu = document.querySelector('.quiz__start-menu')
-
+const quizList = document.querySelector('.quiz-list')
+const quizWindow = document.querySelector('.quiz')
 startButton.addEventListener('click', function (e) { // старт квиза
     e.preventDefault();
 
     startMenu.classList.add('hide')
     quizMenu.classList.remove('hide')
 
-    randomQuestion()
+    let data;
+    switch (startButton.dataset.name) {
+        case 'earth':
+            data = questionsEarth;
+            break;
+        case 'moon':
+            data = questionsMoon;
+            break;
+        case 'space':
+            data = questionsSpace;
+            break;
+    }
+    startQuiz(data);
 })
 
+function startQuiz(data) {
+    randomQuestion(data)
+
+    const validate = () => {
+        if (!optionElements[0].classList.contains('disabled')) {
+            alert('Вам нужно выбрать один из вариантов ответа')
+        } else {
+            randomQuestion(data);
+            enableOptions(data);
+        }
+    }
+    
+    btnNext.addEventListener('click', validate)
+    btnBack.addEventListener('click', () => {
+        window.location.reload()
+    })
+}
+for (button of chooseButton) {
+    button.addEventListener('click', function (e) {
+        e.preventDefault()
+
+        startButton.dataset.name = e.target.dataset.name
+        quizList.classList.add('hide')
+        quizWindow.classList.remove('hide')
+        quizWindow.classList.add('active')
+    })
+}
 const optionElements = document.querySelectorAll('.option')
 
 const option1 = document.querySelector('.option1'), // все ответы
@@ -67,36 +234,36 @@ let score = 0; // итого
 
 const correctAnswer = document.querySelector('.correct') // верный ответ
 
-function load() {
-    numberOfAllQuestions.innerHTML = questions.length;
+function load(data) {
+    numberOfAllQuestions.innerHTML = data.length;
 
-    questionName.innerHTML = questions[indexOfQuestion].question;
+    questionName.innerHTML = data[indexOfQuestion].question;
 
-    option1.innerHTML = questions[indexOfQuestion].options[0];
-    option2.innerHTML = questions[indexOfQuestion].options[1];
-    option3.innerHTML = questions[indexOfQuestion].options[2];
-    option4.innerHTML = questions[indexOfQuestion].options[3];
+    option1.innerHTML = data[indexOfQuestion].options[0];
+    option2.innerHTML = data[indexOfQuestion].options[1];
+    option3.innerHTML = data[indexOfQuestion].options[2];
+    option4.innerHTML = data[indexOfQuestion].options[3];
 
     numberOfQuestion.innerHTML = indexOfPage + 1; //
     indexOfPage++; // увеличение индекса страницы
 }
 let completedAnswers = [];
 
-function randomQuestion() {
-    let randomNumber = Math.floor(Math.random() * questions.length);
+function randomQuestion(data) {
+    let randomNumber = Math.floor(Math.random() * data.length);
 
-    if (completedAnswers.length === questions.length) {
+    if (completedAnswers.length === data.length) {
         quizOver();
         return;
     }
 
     if (completedAnswers.includes(randomNumber)) {
-        randomQuestion();
+        randomQuestion(data);
         return;
     }
 
     indexOfQuestion = randomNumber;
-    load();
+    load(data);
     completedAnswers.push(indexOfQuestion);
 }
 
@@ -104,11 +271,11 @@ function randomQuestion() {
 function quizOver() {
     modal.classList.remove('hide')
     const result = document.querySelector('.quiz-result');
-    result.innerHTML = `Ваш результат ${score} из ${questions.length}`
+    result.innerHTML = `Ваш результат ${score} из ${questionsMoon.length}`
 }
 
 const checkAnswer = el => {
-    if (el.target.dataset.id == questions[indexOfQuestion].correct) {
+    if (el.target.dataset.id == questionsMoon[indexOfQuestion].correct) {
         el.target.classList.add('correct');
         score++;
     } else {
@@ -119,7 +286,7 @@ const checkAnswer = el => {
 function disableOptions() {
     optionElements.forEach(item => {
         item.classList.add('disabled');
-        if (item.dataset.id == questions[indexOfQuestion].correct) {
+        if (item.dataset.id == questionsMoon[indexOfQuestion].correct) {
             item.classList.add('correct');
         }
     })
@@ -135,16 +302,3 @@ for (option of optionElements) {
     })
 }
 
-const validate = () => {
-    if (!optionElements[0].classList.contains('disabled')) {
-        alert('Вам нужно выбрать один из вариантов ответа')
-    } else {
-        randomQuestion();
-        enableOptions();
-    }
-}
-
-btnNext.addEventListener('click', validate)
-btnBack.addEventListener('click', () => {
-    window.location.reload()
-})
